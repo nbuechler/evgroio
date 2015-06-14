75c3175186d7fba3fab5150b41679c228b7f8a98
nbuechler <natebuechler@gmail.com>
2015-06-13 20:59:57 -0400

    Added privacy polict

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+        state('privacy', {
+			url: '/privacy',
+			templateUrl: 'modules/core/views/privacy.client.view.html'
 		});
 	}
 ]);
