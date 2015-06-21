fc25ca4f96c8ceed00977b3792f5f90aeb178ff7
nbuechler <natebuechler@gmail.com>
2015-06-20 20:02:47 -0400

    removed d3 part, added css hover

--- public/modules/logs/directives/new-log-quick.client.directive.js
+++ public/modules/logs/directives/new-log-quick.client.directive.js
@@ -6,17 +6,7 @@ angular.module('logs').directive('newLogQuick', [
 			templateUrl: 'modules/logs/templates/newLogQuickTemplate.html',
 			restrict: 'E',
 			link: function postLink(scope, element, attrs) {
-				// New log quick directive logic
-				// ...
                 
-                d3.select('#newLogQuickButton')
-                .on('mouseover', function() {
-                    d3.select('#newLogQuickButton').style('background-color', '#00A379');
-                })
-                .on('mouseout', function() {
-                    console.log('bye');
-                })
-
 			}
 		};
 	}
