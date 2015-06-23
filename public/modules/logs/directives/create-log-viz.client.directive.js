bb792389c2bfb11b13d8894fb5431fc8ea37d163
nbuechler <natebuechler@gmail.com>
2015-06-22 22:18:26 -0400

    Added a comment of something to do

--- public/modules/logs/directives/create-log-viz.client.directive.js
+++ public/modules/logs/directives/create-log-viz.client.directive.js
@@ -11,6 +11,22 @@ angular.module('logs').directive('createLogViz', [
 			link: function postLink(scope, element, attrs) {
                 
                 d3.select('#physicalDZViz');
+                
+                //TODO: 
+                //Fix this so that it makes dots, or squares, or circles or something next to each
+                //of the five sections.
+                
+                /*
+                 * 1. Try to get the word count/idea length from the scope.
+                 * 2. If not that, try saving the word count/idea length in localstorage
+                 * 3. Take that number and do something with it visually, see above
+                 * 4. Maybe combine all of the data for a big visualization
+                 * 5. Or maybe bring back the idea of a nightengale graph
+                 * 6. Or maybe make a mini dashboard that also includes a spider graph
+                 * 7. But most importantly, try to have fun doing this!
+                 */
+                
+                
 			}
 		};
 	}
