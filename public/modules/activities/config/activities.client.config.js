e301ceb782b85026816c42f5d44a6aebd6fe7108
nbuechler <natebuechler@gmail.com>
2015-08-16 23:40:03 -0400

    Improved UX by using word 'Personal'

--- public/modules/activities/config/activities.client.config.js
+++ public/modules/activities/config/activities.client.config.js
@@ -5,8 +5,8 @@ angular.module('activities').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Activities', 'activities', 'dropdown', '/activities(/create)?');
-		Menus.addSubMenuItem('topbar', 'activities', 'Only my activities', 'activities');
-		Menus.addSubMenuItem('topbar', 'activities', 'Public activities', 'publicActivities');
+		Menus.addSubMenuItem('topbar', 'activities', 'Personal Activities', 'activities');
+		Menus.addSubMenuItem('topbar', 'activities', 'Public Activities', 'publicActivities');
 		Menus.addSubMenuItem('topbar', 'activities', 'New Activity', 'activities/create');
 	}
 ]);
--- public/modules/logs/config/logs.client.config.js
+++ public/modules/logs/config/logs.client.config.js
@@ -5,8 +5,8 @@ angular.module('logs').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Logs', 'logs', 'dropdown', '/logs(/create)?');
-		Menus.addSubMenuItem('topbar', 'logs', 'Only my logs', 'logs');
-		Menus.addSubMenuItem('topbar', 'logs', 'Public logs', 'publicLogs');
+		Menus.addSubMenuItem('topbar', 'logs', 'Personal Logs', 'logs');
+		Menus.addSubMenuItem('topbar', 'logs', 'Public Logs', 'publicLogs');
 		Menus.addSubMenuItem('topbar', 'logs', 'New Log', 'logs/create');
 	}
 ]);
