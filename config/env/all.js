fa70f38099292ba8f028db561c8d74a7ad6a3d31
nbuechler <natebuechler@gmail.com>
2015-08-17 19:55:44 -0400

    Duplicate css, js attrs

--- config/env/all.js
+++ config/env/all.js
@@ -11,39 +11,39 @@ module.exports = {
 	sessionSecret: 'MEAN',
 	sessionCollection: 'sessions',
 	assets: {
-		lib: {
+			lib: {
+					css: [
+			      'public/lib/angular-material/angular-material.css',
+						'public/customLib/bootstrapThemed/darkly/bootstrap.css',
+			    	'public/lib/fontawesome/css/font-awesome.css'
+					],
+					js: [
+						'public/lib/d3/d3.js',
+						'public/lib/angular/angular.js',
+			      'public/lib/angular-aria/angular-aria.js',
+			      'public/lib/angular-material/angular-material.js',
+						'public/lib/angular-resource/angular-resource.js',
+						'public/lib/angular-cookies/angular-cookies.js',
+						'public/lib/angular-animate/angular-animate.js',
+						'public/lib/angular-touch/angular-touch.js',
+						'public/lib/angular-sanitize/angular-sanitize.js',
+						'public/lib/angular-ui-router/release/angular-ui-router.js',
+						'public/lib/angular-ui-utils/ui-utils.js',
+						'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+					]
+			},
 			css: [
-        'public/lib/angular-material/angular-material.css',
-				'public/customLib/bootstrapThemed/darkly/bootstrap.css',
-      	'public/lib/fontawesome/css/font-awesome.css'
+				'public/modules/**/css/*.css'
 			],
 			js: [
-				'public/lib/d3/d3.js',
-				'public/lib/angular/angular.js',
-        'public/lib/angular-aria/angular-aria.js',
-        'public/lib/angular-material/angular-material.js',
-				'public/lib/angular-resource/angular-resource.js',
-				'public/lib/angular-cookies/angular-cookies.js',
-				'public/lib/angular-animate/angular-animate.js',
-				'public/lib/angular-touch/angular-touch.js',
-				'public/lib/angular-sanitize/angular-sanitize.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/config.js',
+				'public/application.js',
+				'public/modules/*/*.js',
+				'public/modules/*/*[!tests]*/*.js'
+			],
+			tests: [
+				'public/lib/angular-mocks/angular-mocks.js',
+				'public/modules/*/tests/*.js'
 			]
-		},
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'public/lib/angular-mocks/angular-mocks.js',
-			'public/modules/*/tests/*.js'
-		]
-	}
+		}
 };
