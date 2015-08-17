cd31b33d1d0820e0838f6ad4f469c962d3c8ea7a
nbuechler <natebuechler@gmail.com>
2015-08-16 22:50:04 -0400

    Activites: added drop down submenu item for public activities

--- public/modules/activities/config/activities.client.config.js
+++ public/modules/activities/config/activities.client.config.js
@@ -5,7 +5,8 @@ angular.module('activities').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Activities', 'activities', 'dropdown', '/activities(/create)?');
-		Menus.addSubMenuItem('topbar', 'activities', 'List Activities', 'activities');
+		Menus.addSubMenuItem('topbar', 'activities', 'Only my activities', 'activities');
+		Menus.addSubMenuItem('topbar', 'activities', 'Public activities', 'publicActivities');
 		Menus.addSubMenuItem('topbar', 'activities', 'New Activity', 'activities/create');
 	}
 ]);
